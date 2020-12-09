55aeda08c2d703cb392de2549ef3059a9beacdc4
Simon Belanger <simon.belanger@ubisoft.com>
2020-12-08 21:15:33 -0500

    Day 05 solution

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -12,6 +12,7 @@ pub mod day01;
 pub mod day02;
 pub mod day03;
 pub mod day04;
+pub mod day05;
 
 pub fn get_puzzle(day: u8) -> Option<Box<dyn Puzzle>> {
     match day {
@@ -19,6 +20,7 @@ pub fn get_puzzle(day: u8) -> Option<Box<dyn Puzzle>> {
         2 => Some(Box::new(day02::create())),
         3 => Some(Box::new(day03::create())),
         4 => Some(Box::new(day04::create())),
+        5 => Some(Box::new(day05::create())),
         _ => None
     }
 }
